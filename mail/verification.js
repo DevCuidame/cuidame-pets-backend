2e19eec5f1d8fa28a9adea5771a003f53ef8439f
oscarCardozoCuidame <ocardozo@cuidame.tech>
2025-04-09 12:13:19 -0500

    Delete mail directory

--- mail/verification.js
+++
@@ -1,3 +0,0 @@
-import { user } from '../controllers/usersController'
-let d = new Date();
-document.body.innerHTML = "<h1>Time right now is:   "+ user + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
