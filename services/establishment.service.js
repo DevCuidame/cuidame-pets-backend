c1fa8b3ad8267c79657040919d3f211dd9408cde
oscarCardozoCuidame <ocardozo@cuidame.tech>
2025-04-09 12:13:56 -0500

    Delete services directory

--- services/establishment.service.js
+++
@@ -1,9 +0,0 @@
-const establishmentRepository = require("../repositories/establishment.repository");
-
-exports.createEstablishment = async (data) => {
-  return establishmentRepository.createEstablishment(data);
-};
-
-exports.getEstablishment = async (id) => {
-  return establishmentRepository.getEstablishment(id);
-};
--- services/legal_representative.service.js
+++
@@ -1,9 +0,0 @@
-const legalRepRepository = require("../repositories/legal_representative.repository");
-
-exports.createLegalRep = async (data) => {
-  return legalRepRepository.createLegalRep(data);
-};
-
-exports.getLegalRep = async (id) => {
-  return legalRepRepository.getLegalRep(id);
-};
