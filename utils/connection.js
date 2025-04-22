ffc3bf67d6154f7e3c51885e81e583aa500d1250
AnthonyLopezz <anthony.lopez@usantoto.edu.co>
2025-04-22 14:36:19 -0500

    Fix connection

--- utils/connection.js
+++ utils/connection.js
@@ -1,20 +1,20 @@
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-//   user: 'esmarthealth',
-//   host: 'localhost',
-//   database: 'esmarthealth',
-//   password: 'eshp.2022',
-//   port: 5432,
-// });
-
 const pool = new Pool({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'db_cuidame',
-  password: 'DataPostGF104',
+  user: 'esmarthealth',
+  host: 'localhost',
+  database: 'esmarthealth',
+  password: 'eshp.2022',
   port: 5432,
 });
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: '127.0.0.1',
+//   database: 'db_cuidame',
+//   password: 'DataPostGF104',
+//   port: 5432,
+// });
 
 
 
